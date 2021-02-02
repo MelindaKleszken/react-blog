821b0d54b5d18471a3e003555d211780d304043f
MelindaKleszken <melindakleszken1021@gmail.com>
2021-02-02 15:20:26 +0000

    dependencies added

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const {postRouter} = require("./routes/post");
 const {userRouter} = require("./routes/user");
 
-const port = process.env.PORT || 3000 || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 //transform to JSON the result
